4ab4c72d139df4d6d397a88932561cc13a22134f
Joost <35542@ma-web.nl>
2023-10-17 15:06:34 +0200

    meer structuur

--- public/index.php
+++ public/index.php
@@ -1,3 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"> 
+    <title>Music</title>
+</head>
+<body>
+<header>
+
+<?php
+?>
+</header>
+
+
+
+
 <?php
 
-phpinfo();
+/**
+ * views folder test.
+ */
+include('../views/data.php');
+
+for ($i = 0; $i < 10; $i++) {
+    
+    foreach ($dataLibrary as $musicItem) {
+        echo $musicItem['id'] . "br";
+
+       
+
+    }
+}
+
+
+
+
+
+
+?>
+</body>
+</html>
+
+
---
+++ views/data.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * Test voor views folder
+ */
+$dataLibrary = 
+    [
+        [
+            "id" => 1,
+            "title" => "Title placeholder",
+            "duration" => "...",
+            "artist" => "Artist",
+            "jaar" => "201900",
+            "image" => "img/bla bla.webp",
+
+        ],
+
+        [
+            "id" => 2,
+            "title" => "bla",
+            "artist" => "blas",
+            "jaar" => "bla",
+            "image" => "img/bla bla.webp",
+
+        ],
+
+        [
+            "id" => 4,
+            "title" => "bla",
+            "artist" => "blas",
+            "jaar" => "bla",
+            "image" => "img/bla bla.webp",
+
+        ],
+        [
+            "id" => 5,
+            "title" => "bla",
+            "artist" => "blas",
+            "jaar" => "bla",
+            "image" => "img/bla bla.webp",
+
+        ],
+        [
+            "id" => 6,
+            "title" => "bla",
+            "artist" => "blas",
+            "jaar" => "bla",
+            "image" => "img/bla bla.webp",
+
+        ],
+        [
+            "id" => 7,
+            "title" => "bla",
+            "artist" => "blas",
+            "jaar" => "bla",
+            "image" => "img/bla bla.webp",
+
+        ],
+        [
+            "id" => 8,
+            "title" => "bla",
+            "artist" => "blas",
+            "jaar" => "bla",
+            "image" => "img/bla bla.webp",
+
+        ],
+        [
+            "id" => 9,
+            "title" => "bla",
+            "artist" => "blas",
+            "jaar" => "bla",
+            "image" => "img/bla bla.webp",
+
+        ],
+        [
+            "id" => 10,
+            "title" => "bla",
+            "artist" => "blas",
+            "jaar" => "bla",
+            "image" => "img/bla bla.webp",
+
+        ],
+      
+
+
+        
+    ]
+        
+    
+
+
+
+
+?>
